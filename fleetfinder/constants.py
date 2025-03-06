7750988ac619cfa8825b34b6b0a4f718a5ff7f02
Peter Pfeufer <info@ppfeufer.de>
2025-03-06 22:03:31 +0100

    [RELEASE] v2.3.2
    ## [2.3.2] - 2025-03-06
    
    ### Changed
    
    - Templatetag code improved
    - Omit SRI hash calculation when `DEBUG=True`
    - Translations updated

--- fleetfinder/constants.py
+++ fleetfinder/constants.py
@@ -12,9 +12,11 @@ from esi import __version__ as esi_version
 from fleetfinder import __version__
 
 APP_NAME = "aa-fleetfinder"
+APP_NAME_VERBOSE = "AA Fleet Finder"
+APP_NAME_VERBOSE_USERAGENT = "AA-Fleet-Finder"
 PACKAGE_NAME = "fleetfinder"
 GITHUB_URL = f"https://github.com/ppfeufer/{APP_NAME}"
-USER_AGENT = f"{APP_NAME}/{__version__} ({GITHUB_URL}) via django-esi/{esi_version}"
+USER_AGENT = f"{APP_NAME_VERBOSE_USERAGENT}/{__version__} (+{GITHUB_URL}) Django-ESI/{esi_version}"
 
 APP_BASE_DIR = os.path.join(os.path.dirname(__file__))
 APP_STATIC_DIR = os.path.join(APP_BASE_DIR, "static", PACKAGE_NAME)
